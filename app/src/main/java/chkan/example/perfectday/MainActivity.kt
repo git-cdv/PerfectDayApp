68325fa4a15fb4c2f5e6b0d18af14b9a01a6c63c
chkan <chkan.dv@gmail.com>
2024-05-06 18:58:30 +0300

    Init + Splash screen

---
+++ app/src/main/java/chkan/example/perfectday/MainActivity.kt
@@ -0,0 +1,89 @@
+package chkan.example.perfectday
+
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.splashscreen.SplashScreenViewProvider
+import chkan.example.perfectday.ui.theme.PerfectDayTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                !viewModel.isReady.value
+            }
+            setOnExitAnimationListener{ screen ->
+                startExitAnimation(screen)
+            }
+        }
+        enableEdgeToEdge()
+        setContent {
+            PerfectDayTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+
+    private fun startExitAnimation(screen: SplashScreenViewProvider) {
+        val zoomX = ObjectAnimator.ofFloat(
+            screen.iconView,
+            View.SCALE_X,
+            0.4f,
+            0.0f
+        )
+        zoomX.interpolator = OvershootInterpolator()
+        zoomX.duration = 500L
+        zoomX.doOnEnd { screen.remove() }
+
+        val zoomY = ObjectAnimator.ofFloat(
+            screen.iconView,
+            View.SCALE_Y,
+            0.4f,
+            0.0f
+        )
+        zoomY.interpolator = OvershootInterpolator()
+        zoomY.duration = 500L
+        zoomY.doOnEnd { screen.remove() }
+
+        zoomX.start()
+        zoomY.start()
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PerfectDayTheme {
+        Greeting("Android")
+    }
+}
